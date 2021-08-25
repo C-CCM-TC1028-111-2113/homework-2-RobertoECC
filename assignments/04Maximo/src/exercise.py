57dbbb31c52cf39b77a2dfcd0c31faaf554b8669
RobertoECC <88851027+RobertoECC@users.noreply.github.com>
2021-08-24 22:01:21 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,7 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+num1 = int(input("Escribe el primer número: "))
+num2 = int(input("Escribe el segundo número: "))
+num3 = int(input("Escribe el tercer número: "))
 
 
+if (num1>num2) and (num1>num3):
+	print(num1)
+elif (num2>num1) and (num2>num3):
+	print (num2)
+elif (num3>num1) and (num3>num2):
+	print(num3)
+    
 if __name__=='__main__':
     main()
